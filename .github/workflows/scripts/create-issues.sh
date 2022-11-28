3e6121fc8ead8f8072ba5572a420c5d88a39fb8e
tcskill <tskill@us.ibm.com>
2022-11-28 17:05:37 -0600

    flagfx

--- .github/workflows/scripts/create-issues.sh
+++ .github/workflows/scripts/create-issues.sh
@@ -7,13 +7,13 @@ https://api.github.com/notifications)
 
 # for each notification check if it's a release notification for the repo we care about
 for row in $(echo "${resp}" | jq -r '.[] | @base64'); do
-    type=$(echo ${row} | base64 --d | jq -r '.subject.type' )
-    repo=$(echo ${row} | base64 --d | jq -r '.repository.full_name' )
+    type=$(echo ${row} | base64 -d | jq -r '.subject.type' )
+    repo=$(echo ${row} | base64 -d | jq -r '.repository.full_name' )
 
     if [[ $type == "Release" ]] && [[ $repo == "ibm-mas/ansible-devops" ]]; then
 
-        title=$(echo ${row} | base64 --d | jq -r '.subject.title' )
-        threadid=$(echo ${row} | base64 --d | jq -r '.id' )
+        title=$(echo ${row} | base64 -d | jq -r '.subject.title' )
+        threadid=$(echo ${row} | base64 -d | jq -r '.id' )
 
 # for release notificaitons, build the data for the issue to open
         _issue_data()
